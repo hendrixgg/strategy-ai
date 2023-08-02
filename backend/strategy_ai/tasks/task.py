e9a05536b86724699fcea205c4295cce9f19321e
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-08-02 13:14:12 -0400

    rewrote BaseTask into Object-functional style

--- backend/strategy_ai/tasks/task.py
+++ backend/strategy_ai/tasks/task.py
@@ -1,133 +1,163 @@
-from abc import ABC, abstractmethod
-from uuid import uuid4
+import uuid
 from enum import Enum
-from dataclasses import dataclass, asdict
 import datetime
 import json
 import pickle
 import os
+from typing import Iterator, Optional, Callable
+from pydantic import BaseModel, Field
 
-from strategy_ai.tasks.path_to_json import path_to_dict
 
-
-class TaskStatus(Enum):
+class TaskState(Enum):
     PREPARING: str = "preparing"
     READY: str = "ready"
     RUNNING: str = "running"
     FINISHED: str = "finished"
 
-    def __get__(self, instance, owner):
-        return self.value
+
+class TaskType(BaseModel):
+    name: str = Field(description="The name of the task")
+    id: int = Field(description="The id of the task")
 
 
-@dataclass
-class TaskResponse():
-    """Class for keeping track of the task api responses"""
-    status: TaskStatus
-    message: str
-    date: str
-    id: str
-    task_type_id: int
-    task_name: str
-    # Should be what you would print to the console indicating what progress has been made so far, this variable can change over the course of running the task.
-    progress_info: str
-    # Depends on the type of task, could be a string for text outputs, an of object for a file, etc. This variable may change over the course of running the task.
-    results: str
-    # folder indicating all the files that were available during the task execution
-    files_available: dict
-    # properties about the task being run, such as the parameters chosen to run the task, the date the task was run, etc.
-    metadata: dict
-
-    def update(self):
-        self.date = str(datetime.datetime.now())
+class TaskTypeEnum(TaskType, Enum):
+    SURFACING: TaskType = TaskType(name="t1_surfacing", id=1)
+    ASSESSMENT: TaskType = TaskType(name="t2_assessment", id=2)
 
 
-class BaseTask(ABC):
-    def __init__(self, task_type: int, task_name: str, availableDataFolder: str):
-        """must provide the task type and name.
-        You also need to implement the generate_results method
-        which is an iterator that will output the results of 
-        running the task as it progresses"""
-        self.currentResponse = TaskResponse(
-            status=TaskStatus.PREPARING,
-            message="",
-            date=str(datetime.datetime.now()),
-            task_uuid=str(uuid4()),
-            task_type_id=task_type,
-            task_name=task_name,
-            progress_info="",
-            results="",
-            files_available=path_to_dict(availableDataFolder, ""),
-            metadata=dict(),
-        )
-        self.runHistory: list[tuple[datetime.datetime, dict]] = []
-        self.detailedResults = {}
+class TaskData(BaseModel):
+    """Class for keeping track of a task's data"""
+    state: TaskState = Field(default=TaskState.PREPARING,
+                             description="The current state of the task")
+    message: str = Field(default="",
+                         description="The current message of what the task is doing")
+    date_start: Optional[datetime.datetime] = Field(default=None,
+                                                    description="The date the task was started")
+    date_recent: datetime.datetime = Field(default_factory=datetime.datetime.now,
+                                           description="The date the task was last updated")
+    id: uuid.UUID = Field(default_factory=uuid.uuid4,
+                          description="The unique id of the task")
+    task_type: TaskTypeEnum = Field(description="The type of task",
+                                    frozen=True)
+    progress_info: str = Field(default="",
+                               description="Should be what you would print to the console indicating what progress has been made so far, this variable can change over the course of running the task.")
+    results_text: str = Field(default="",
+                              description="A string for text outputs of the task results. This will typically be in markdown format.")
+    files_available: dict = Field(default_factory=dict,
+                                  description="A dict representing a folder that contains all the files that were available during the task execution")
+    run_history: list[tuple[datetime.datetime, dict]] = Field(default_factory=list,
+                                                              description="stores each message from the run history of the task")
+    detailed_results: dict = Field(default_factory=dict,
+                                   description="stores the detailed results of the task, the data structure is task specific")
 
-    @abstractmethod
-    def generate_results(self):
-        """This function will return a generator that will yield json strings that can be sent to the frontend."""
-        if self.currentResponse.status == TaskStatus.PREPARING:
-            raise Exception(
-                f"Not ready to run task {self.currentResponse.task_name}, uuid: {self.currentResponse.task_uuid}.")
-        elif self.currentResponse.status == TaskStatus.READY:
-            self.currentResponse.message = f"Starting task {self.currentResponse.task_name}, uuid: {self.currentResponse.task_uuid}."
-        elif self.currentResponse.status == TaskStatus.RUNNING:
-            raise Exception(
-                f"Already running, task {self.currentResponse.task_name}, uuid: {self.currentResponse.task_uuid}.")
-        else:  # FINISHED
-            raise Exception(
-                f"Already finished, task {self.currentResponse.task_name}, uuid: {self.currentResponse.task_uuid}.")
+    class Config:
+        json_encoders = {
+            datetime.datetime: lambda dt: dt.isoformat(),
+            uuid.UUID: lambda u: str(u),
+        }
+        use_eunm_values = True
 
-        self.currentResponse.status = TaskStatus.RUNNING
-        yield {"type": "message", "body": self.currentResponse.message}
 
-    def generate_results_json_bytes(self, saveDirectory: str | None = None):
-        """This function will run the task, returning a generator that will yield json strings preceded by newline characters that can be sent to the frontend. All results will be saved with this task and stored in the saveDirectory upon completion.
+def task_save(task: TaskData, directory: str):
+    """This function will save the results of the task to the given directory.
 
-        Additional things to note:
-        - This function will also update the currentResponse object as the task progresses.
-        - This function will append each json string to the runHistory object.
-
-        The json strings will have the following format:
-        - result_text: will be appended to the currentResponse.results variable preceded by a newline character.
-        - progress_info: will be appended to the currentResponse.progress_info variable preceded by a newline character.
-        - message: will be assign to the currentResponse.message variable.
+    Inside the given directory, the save file will contain:
+    - the task's detailed results (pickle file ".pkl")
+    - the task's run history (csv file ".csv")
+    - the readable copy of the results (markdown text file ".md").
         """
-        for result in self.generate_results(saveDirectory):
-            self.runHistory.append((datetime.datetime.now(), result))
-            match result["type"]:
-                case "results_text":
-                    self.currentResponse.results += result["body"] + "\n"
-                case "progress_info":
-                    self.currentResponse.progress_info += result["body"] + "\n"
-                case "message":
-                    self.currentResponse.message = result["body"]
-            self.currentResponse.update()
-            jsonString = json.dumps(result) + "\n"
-            yield bytes(jsonString, encoding="ascii")
+    if TaskData.state != TaskState.FINISHED:
+        raise Exception(
+            "Cannot save the results yet since the task is not finished")
 
-    def save(self, directory: str):
-        """This function will save the results of the task to the given directory.
+    new_directory = os.path.join(
+        directory, f"{task.task_type.name}-{task.task_type.id}")
+    os.mkdir(new_directory)
 
-        Inside the given directory, the save file will contain:
-        - the task's detailed results (pickle file ".pkl")
-        - the task's run history (csv file ".csv")
-        - the readable copy of the results (markdown text file ".md").
-         """
-        if self.currentResponse.status != TaskStatus.FINISHED:
-            raise Exception(
-                "Cannot save the results yet since the task is not finished")
+    with open(file=os.path.join(new_directory, "detailedResults.pkl"), mode="wb") as f:
+        pickle.dump(task.detailed_results, file=f)
 
-        new_directory = os.path.join(
-            directory, f"{self.currentResponse.task_name}-{self.currentResponse.task_uuid}")
-        os.mkdir(new_directory)
+    with open(file=os.path.join(new_directory, "runHistory.csv"), mode="w", newline="\n") as f:
+        for time, entry in task.run_history:
+            print(f'{time},"{entry}"', file=f)
 
-        with open(file=os.path.join(new_directory, "detailedResults.pkl"), mode="wb") as f:
-            pickle.dump(self.detailedResults, file=f)
+    with open(file=os.path.join(new_directory, "readableResults.md"), mode="w", newline="\n") as f:
+        print(task.results_text, file=f)
 
-        with open(file=os.path.join(new_directory, "runHistory.csv"), mode="w", newline="\n") as f:
-            for time, entry in self.runHistory:
-                print(f'{time},"{entry}"', file=f)
 
-        with open(file=os.path.join(new_directory, "readableResults.md"), mode="w", newline="\n") as f:
-            print(self.currentResponse.results, file=f)
+def task_update(task: TaskData) -> bool:
+    """Updates the task with the current datetime, if the task is not already finished.
+    Args:
+        task: A task.TaskData instance to be updated.
+
+    Returns: 
+        True if the task was updated, False if the task was already finished.
+
+    Raises:
+        None.
+    """
+    if task.state != TaskState.FINISHED:
+        task.date_recent = datetime.datetime.now()
+        return True
+    else:
+        return False
+
+
+def task_generate_results_before(task: TaskData) -> Iterator[dict]:
+    """This function will return a generator that will yield the first few dictionaries that can be sent to the frontend.
+
+    Args:
+        `task`: a TaskData instance to be referenced.
+
+    Returns:
+        A generator that will yield the first few dictionaries that can be sent to the frontend.
+
+    Raises:
+        Exception: If the task.state != TaskState.Ready.
+    """
+    if task.state == TaskState.PREPARING:
+        raise Exception(
+            f"Not ready to run task {task.task_type.name}, uuid: {task.id}.")
+    elif task.state == TaskState.READY:
+        task.message = f"Starting task {task.task_type.name}, uuid: {task.id}."
+    elif task.state == TaskState.RUNNING:
+        raise Exception(
+            f"Already running, task {task.task_type.name}, uuid: {task.id}.")
+    else:  # FINISHED
+        raise Exception(
+            f"Already finished, task {task.task_type.name}, uuid: {task.id}.")
+
+    task.state = TaskState.RUNNING
+    task.date_start = datetime.datetime.now()
+    yield {"type": "message", "body": task.message}
+
+
+def task_generate_results_processing(task: TaskData, task_results_generator: Callable[[TaskData], Iterator[dict]]) -> Iterator[dict]:
+    """This function will update the task as the results are generated and yielded.
+
+    Args:
+        `task`: TaskData instance to be referenced. This is the task that is being run.
+
+        `task_results_generator`: A function that takes in a TaskData instance and returns a generator that yields dictionaries that can both be used to update `task` and can be sent to the frontend.
+
+    Returns:
+        A generator of dicts which are all the results of the task.
+
+    Raises:
+        None.
+    """
+    for result in task_results_generator(task):
+        match result["type"]:
+            case "results_text":
+                task.results_text += result["body"] + "\n"
+            case "progress_info":
+                task.progress_info += result["body"] + "\n"
+            case "message":
+                task.message = result["body"]
+        task_update(task)
+        yield result
+
+
+def dict_iter_ndjson_bytes(dict_iter: Iterator[dict]) -> Iterator[bytes]:
+    for d in dict_iter:
+        yield bytes(json.dumps(d) + "\n", encoding="ascii")
